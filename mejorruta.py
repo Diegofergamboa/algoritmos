f734070d39bcb749ba5bdb48a8662d5bd65bea79
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-11-17 21:18:48 -0500

    Proyecto para generar un algoritmo que dadas las coordenadas de ubicacion y, las distancias entre lugares, nos permita identificar cual es la mejor ruta para llegar a un destino

---
+++ mejorruta.py
@@ -0,0 +1,21 @@
+''' 
+    Adecuacion del algoritmo por medio del cual se va a calcular la mejor ruta para de un lugar a otro
+    Algoritmo adyacente
+    #### El problemas consiste en evaluar cual es la mejor ruta de mi casa al gimnasio ####
+'''
+
+# Que datos necesito = Distancia
+# Salida = Cual es la mejor ruta
+# Por datos
+
+# Comparador logico
+
+escuela = ['coordenada' = 1,0, 'distancia_casa' = 10] #En metros
+casa = ['coordenada' = 0,0, 'distancia_casa' = 0]
+restaurante = ['coordenada' = 0,1 'distancia_casa' = 5]
+gimnasio = ['coordenada' = 1,0, 'distancia_casa' = 15]
+
+
+def distancia()
+    
+    
